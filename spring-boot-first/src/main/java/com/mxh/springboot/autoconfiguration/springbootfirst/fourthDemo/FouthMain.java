0c52d581ef801bf5ae1e7f50fe2f3bf3b221ed84
maxiaohong123 <554349712@qq.com>
2019-10-18 17:40:08 +0800

    'first'

--- spring-boot-first/src/main/java/com/mxh/springboot/autoconfiguration/springbootfirst/fourthDemo/FouthMain.java
+++ spring-boot-first/src/main/java/com/mxh/springboot/autoconfiguration/springbootfirst/fourthDemo/FouthMain.java
@@ -1,13 +1,14 @@
 package com.mxh.springboot.autoconfiguration.springbootfirst.fourthDemo;
 
 import com.mxh.core.MxhCore;
-import com.mxh.springboot.autoconfiguration.springbootfirst.thirdDemo.CacheService;
-import com.mxh.springboot.autoconfiguration.springbootfirst.thirdDemo.LoggerService;
-import com.mxh.springboot.autoconfiguration.springbootfirst.thirdDemo.ThirdMain;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+/**
+ * demo4:本例是测试springboot的 EnableAutoConfiguration 、ConditionalOnClass...(条件注解)。
+ *      结合另外一个小demo :mxh-gupaocore
+ */
 @SpringBootApplication
 public class FouthMain {
     public static void main(String[] args) {
