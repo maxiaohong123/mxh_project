17d0974a71a1294b55eb79be1931b7eb71f61f40
maxiaohong123 <554349712@qq.com>
2019-12-24 18:13:16 +0800

    'gateway手写demoInit'

---
+++ servlet-gateway/src/test/java/com/mxh/gateway/servlet/ServletGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mxh.gateway.servlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
